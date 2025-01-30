2b6141b2f8ef7d2422c875cdf9bbc2fb75df0038
Samuel Custódio <samuel.rodrigues@aluno.unievangelica.edu.br>
2025-01-30 15:38:11 -0300

    #updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
     },
     extend: {
       colors: {
-        'black': '#000',
-        'whatsapp': '#25d366',
+        black: "#000",
+        whatsapp: "#25d366",
       },
-      colorScheme: {
-      },
+      colorScheme: {},
       backgroundImage: {
-        'login': "url('/images/login-bg.png')",
-        'gradient-bg': 'linear-gradient(90deg, #0A5364 0%, #094756 17.01%, #0B1B1A 67.51%, #0E110A 90.06%)',
-        'gradient-md': 'linear-gradient(180deg, #002B35, #007E9B)',
+        login: "url('/images/login-bg.png')",
+        "gradient-bg":
+          "linear-gradient(90deg, #0A5364 0%, #094756 17.01%, #0B1B1A 67.51%, #0E110A 90.06%)",
+        "gradient-md": "linear-gradient(180deg, #002B35, #007E9B)",
       },
       fontFamily: {
-        'montserrat': ['Montserrat', 'sans-serif'],
+        montserrat: ["Montserrat", "sans-serif"],
       },
     },
   },
-  plugins: [
-  ]
-}
-
-
+  plugins: [],
+};
 
 /*
 'btnBlue': '#162778',
